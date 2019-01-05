c0215154f674008c8c6276fd933d3e9ecfd553fe
KRISTIYAN GEORGIEV VLADIMIROV <krizs.vladimirov907@gmail.com>
2019-01-05 22:47:45 +0200

    Added models and views to posts

---
+++ MyBlog/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Models
+{
+    public class Comment
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required]
+        public string CommentValue { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime CreationDate { get; set; }
+        
+        public virtual ApplicationUser Owner { get; set; }
+       
+        public virtual Post BelongingPost { get; set; }
+    }
+}
---
+++ MyBlog/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Models
+{
+    public class Post
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "The post's content")]
+        public string PostValue { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime CreationDate { get; set; }
+        
+        public virtual ApplicationUser Owner { get; set; }
+    }
+}
